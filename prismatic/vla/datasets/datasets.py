0de3e66b74aca9294d98b49d6bee0440843f8a78
tom <tthayer93@gmail.com>
2026-07-05 00:30:39 -0700

    perf: remove unused dataset_name from RLDS batch transform

--- prismatic/vla/datasets/datasets.py
+++ prismatic/vla/datasets/datasets.py
@@ -64,7 +64,7 @@ class RLDSBatchTransform:
         if not self.predict_stop_token:
             labels[-1] = IGNORE_INDEX
 
-        return dict(pixel_values=pixel_values, input_ids=input_ids, labels=labels, dataset_name=dataset_name)
+        return dict(pixel_values=pixel_values, input_ids=input_ids, labels=labels)
 
 
 class RLDSDataset(IterableDataset):
